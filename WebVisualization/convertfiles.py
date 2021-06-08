14dac5db7e23881e714ccb8dd0d605f5b74fc418
Geoffrey Flynn <gflynn@kmtel.com>
2021-06-07 20:53:13 -0500

    grid

---
+++ WebVisualization/convertfiles.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+
+filepath = "Resources/output_data/city_temperature.csv"
+fileout = "Resources/output_data/city_temperature.html"
+df = pd.read_csv(filepath)
+
+df.to_html(fileout)
