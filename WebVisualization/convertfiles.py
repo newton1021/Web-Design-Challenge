63888cd565565311c8396adabe3b75bd381fa1e1
Geoffrey Flynn <gflynn@kmtel.com>
2021-06-08 17:55:55 -0500

    bootstrap initial complete :

--- WebVisualization/convertfiles.py
+++ WebVisualization/convertfiles.py
@@ -7,4 +7,4 @@ filepath = "Resources/output_data/city_temperature.csv"
 fileout = "Resources/output_data/city_temperature.html"
 df = pd.read_csv(filepath)
 
-df.to_html(fileout)
+df.to_html(fileout, index=False)
